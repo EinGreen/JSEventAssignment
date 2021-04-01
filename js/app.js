415680110938248809ca0d6b7eb3752bac57f26e
Ein_Green <mbananola2002@gmail.com>
2021-04-01 00:00:16 -0700

    created 2 boxes that change the html of the page with js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// these are global scoped variables, anyone can use this
+var defaultnum = 0;
+
+// this function turns the background a shade green because why not?
+function clickThis(eventDetails) {
+    console.log(eventDetails);
+    document.body.style.background = "lime";
+    whyThis.removeEventListener("click", clickThis);
+}
+
+// this block of code makes it so when people click the "click me" box, the function above happens 
+let whyThis = document.getElementById(`why`);
+whyThis.addEventListener("click", clickThis);
+
+// this block of useless code increases the number within another box over and over again
+function incrementNumber(eventDetails) {
+    console.log(eventDetails);
+    defaultnum++;
+    document.getElementById("counter").innerText = defaultnum;
+}
+
+// this block of code makes it so the useless code above gets activated
+let hoverThisIdiot = document.getElementById(`hoverThis`);
+hoverThisIdiot.addEventListener(`mousemove`, incrementNumber);
+
