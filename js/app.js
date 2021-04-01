0185ac00210d80827a25cbd0f4f84aee8b67326e
Ein_Green <mbananola2002@gmail.com>
2021-03-31 22:50:38 -0700

    RickRoll didn't work, try something else

--- js/app.js
+++
@@ -1,6 +0,0 @@
-function rickRoll() {
-    document.getElementById(`rickRoll`).innerText(`told ya`);
-}
-
-let rickRoll = document.getElementById(`rickRoll`);
-rickRoll.addEventListener("mouseover", rickRoll());
